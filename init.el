5081de2936c7918f8a11c1ac43069782292f4cc1
Kyle Cotton <kylecottonkc@gmail.com>
2018-02-27 16:47:51 +0000

    Added archive to datetree functionality. General changes as well as change to agenda.

--- init.el
+++ init.el
@@ -5,17 +5,3 @@
 
 (org-babel-load-file "~/.emacs.d/config.org")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yasnippet expand-region undo-tree ace-window auto-complete avy counsel org-bullets which-key try zenburn-theme use-package bind-key))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
