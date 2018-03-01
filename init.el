a06467ac941bd32d650454281aac64b6ff30669e
Kyle Cotton <kylecottonkc@gmail.com>
2018-03-01 22:56:36 +0000

    WORKING VERSION. Previous commit had an error I wasn't aware of.

--- init.el
+++ init.el
@@ -1,7 +1,2 @@
-;; (require 'package)
-;; (add-to-list 'package-archives
-;;              '("melpa" . "http://melpa.org/packages/") t)
-;; (package-initialize)
-
 (org-babel-load-file "~/.emacs.d/config.org")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
