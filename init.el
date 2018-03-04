f1f76643c5bbffe843a3b30ab456a75a74b3f5ee
Kyle Cotton <kylecottonkc@gmail.com>
2018-03-04 20:04:34 +0000

    Started Using/Testing dakrone-theme.

--- init.el
+++ init.el
@@ -7,17 +7,3 @@
 
 (org-babel-load-file "~/.emacs.d/config.org")
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (org undo-tree all-the-icons-dired zenburn-theme yasnippet which-key use-package try powerline org-bullets expand-region dired-sidebar counsel auto-complete ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
