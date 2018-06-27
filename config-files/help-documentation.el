682e4d9a7b9f1eaa4f34900f5ca38d71b2f2ec6a
Kyle Cotton <kylecottonkc@gmail.com>
2018-06-27 14:59:52 +0100

    Removed all files with old convention

--- config-files/help-documentation.el
+++
@@ -1,5 +0,0 @@
-;;Which Key
-(use-package which-key
-	:ensure t
-	:config
-	(which-key-mode))
--- config-files/os-specific.el
+++
@@ -1,7 +0,0 @@
-;;MacOS
-;;############################
-;;Realease Modifier Keys
-(setq ns-right-alternate-modifier (quote noneaa))
-(setq ns-right-option-modifier (quote none))
-(setq ns-right-command-modifier (quote none))
-(setq ns-right-control-modifier (quote none))
