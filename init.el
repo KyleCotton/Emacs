7bfb3b9c2c3080136ff0e0d76dd443a3f2fc3d01
Kyle Cotton <kylecottonkc@gmail.com>
2018-06-27 15:00:20 +0100

    Chagged init.el & config.org to match new convention.

--- init.el
+++ init.el
@@ -1,30 +1,45 @@
-;;(package-initialize)
-(add-to-list 'load-path "~/.emacs.d/config-files")
-(load-library "setup")
-(load-library "interface")
-(load-library "os-specific")
-(load-library "vc-backup")
-(load-library "modeline")
-(load-library "text-manipulation")
-(load-library "text-navigation")
-(load-library "help-documentation")
-(load-library "window-buffer-navigation")
-(load-library "org-misc")
-(load-library "org-agenda")
-(load-library "org-todo")
-(load-library "org-export")
-(load-library "org-agenda-custom-view")
-(load-library "org-capture.el")
+(add-to-list 'load-path "~/.emacs.d/config-files/")
+(require 'init-setup)
+(require 'init-secrets)
+(require 'init-org-setup)
+(require 'init-interface)
+(require 'init-interface-theme)
+(require 'init-os-specific)
+(require 'init-vc-backup)
+(require 'init-modeline)
+(require 'init-text-manipulation)
+(require 'init-text-navigation)
+(require 'init-help-documentation)
+(require 'init-window-buffer-navigation)
+(require 'init-org-misc)
+(require 'init-org-agenda)
+(require 'init-org-todo)
+(require 'init-org-export)
+(require 'init-org-agenda-custom-view)
+(require 'init-org-capture)
+(require 'init-irc)
+(require 'init-org-calendar)
+;;(require 'init-org-calendar-calfw)
 
 
-;;(load-library "org-calender") Not written yet
 
+;;(message "FreeNode Password")
+;;(message freenode-password)
 
-;;(load-library "")
-;;(load-library "")
-;;(load-library "")
-;;(load-library "")
-
-;;(org-babel-load-file "~/.emacs.d/config.org")
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ;;(load-library "")
+;; ;;(org-babel-load-file "~/.emacs.d/config.org")
+;-----------------------------------------------------------------------
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (calfw-gcal zenburn-theme yasnippet which-key use-package undo-tree try powerline org-gcal org-bullets magit htmlize expand-region doom-themes counsel auto-complete ace-window))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :height 3.0)))))
