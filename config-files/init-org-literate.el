9713bf7c43fbd71435ff81548b26da37eb660cb4
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-18 00:06:52 +0100

    Added Literate Programming Config

---
+++ config-files/init-org-literate.el
@@ -0,0 +1,8 @@
+;;This code allow for literate programming in org mode.
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (java . t)))
+
+
+(provide 'init-org-literate)
