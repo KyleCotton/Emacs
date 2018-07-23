4b60a5992ddf2c7f300cded8ef3f08dbd3a6c87e
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-23 19:27:22 +0100

    Updated Modeline File

--- config-files/init-modeline.el
+++ config-files/init-modeline.el
@@ -1,18 +1,29 @@
 ;;Mode Line
 (display-time-mode 1)
 
-(use-package powerline
+;; (use-package powerline
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (require 'cl)
+;;     (powerline-default-theme)
+;;     (setq powerline-arrow-shape 'arrow)))
+ (use-package powerline
+   :ensure t
+   :init
+   (powerline-default-theme))
+
+(use-package airline-themes
   :ensure t
   :init
-  (progn
-    (require 'cl)
-    (powerline-default-theme)
-    (setq powerline-arrow-shape 'arrow)))
+  (load-theme 'airline-light))
 
-  ;; (use-package airline-themes
-  ;;   :ensure t
-  ;;   :init
-  ;;   (progn
-;;     (load-theme 'airline-dark)))
+;; (use-package powerline-evil
+;;   :ensure t
+;;   :init
+;;   (progn
+;;     (require 'cl)
+;;     (powerline-default-theme)
+;;     (setq powerline-arrow-shape 'arrow)))
 
 (provide 'init-modeline)
