f14f2368e567fd1ade734b4c0c05a3d3c22610f4
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-26 11:45:22 +0100

    Added Helm Functionality

---
+++ config-files/init-helm.el
@@ -0,0 +1,20 @@
+;; Helm Configuration
+
+;; (use-package helm
+;;   :ensure t)
+
+;; (use-package helm-config
+;;   :ensure t)
+
+(use-package helm-swoop
+  :ensure t)
+
+;; (use-package helm-find-files
+;;   :ensure t)
+
+
+(global-set-key (kbd "C-s") 'helm-swoop)
+;;(global-set-key (kbd "C-x C-f") 'helm-find-files)
+
+
+(provide 'init-helm)
