a94f88006d98201447fe80eae1f339860ae1d42b
Kyle Cotton <kylecottonkc@gmail.com>
2018-07-26 15:05:51 +0100

    Added highlighting matching brackets.

--- config-files/init-misc.el
+++ config-files/init-misc.el
@@ -1,7 +1,12 @@
 ;; Misc Behaviour
 
 ;; Replace Text that is highlighted
-
 (delete-selection-mode 1)
+
+;; Highlight Maching Bracket
+(show-paren-mode t)
+(setq show-paren-style 'expression)
+
+
 
 (provide 'init-misc)
