6bb0b259989fcf83dfdb1539941d4bc799456a9e
Kyle Cotton <kylecottonkc@gmail.com>
2018-08-12 15:00:09 +0100

    Added Ibuffer groups

--- config-files/init-window-buffer-navigation.el
+++ config-files/init-window-buffer-navigation.el
@@ -1,3 +1,5 @@
+;; Window and buffer navigation configuration
+
 ;;Ace Window
 (use-package ace-window
   :ensure t
@@ -14,6 +16,34 @@
 
 ;;IBuffer List Buffers
 (defalias 'list-buffers 'ibuffer-other-window)
+
+;; Groups for specific buffer types
+
+(setq ibuffer-saved-filter-groups
+      '(("home"
+         ("irc" (mode . circe-channel-mode))
+	 ("Org" (or (mode . org-mode)
+		    (filename . "OrgMode")))
+	 ("Development" (or (mode . shell-mode)
+			(mode . eshell-mode)))
+	 ("Magit" (mode . magit-mode))
+	 ("ERC" (mode . erc-mode))
+	 ("Help" (or (name . "\*Help\*")
+		     (name . "\*Apropos\*")
+		     (name . "\*info\*"))))))
+
+
+(add-hook 'ibuffer-mode-hook
+	  '(lambda ()
+	     (ibuffer-auto-mode 1)
+	     (ibuffer-switch-to-saved-filter-groups "home")))
+
+(setq ibuffer-show-empty-filter-groups nil)
+
+(add-hook 'ibuffer-mode-hook
+	  (lambda ()
+	    (ibuffer-auto-mode 1)
+	    (ibuffer-switch-to-saved-filter-groups "default")))
 
 
 (provide 'init-window-buffer-navigation)
