3e158dfb2d56d7fe557c7c1f2fd2c3ff5233c574
Kyle Cotton <kylecottonkc@gmail.com>
2018-10-27 12:06:36 +0100

    Added spotify config

---
+++ config-files/init-spotify.el
@@ -0,0 +1,7 @@
+(use-package counsel-spotify
+  :ensure t)
+
+(setq counsel-spotify-client-id spotify-client-id)
+(setq counsel-spotify-client-secret spotify-client-secret)
+
+(provide 'init-spotify)
