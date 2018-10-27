441e1c9bf008452227131cdb7d4d2c43eec39ccd
Kyle Cotton <kylecottonkc@gmail.com>
2018-10-27 12:27:54 +0100

    Added new languages for literate programming

--- config-files/init-org-literate.el
+++ config-files/init-org-literate.el
@@ -1,8 +1,14 @@
 ;;This code allow for literate programming in org mode.
 (org-babel-do-load-languages
  'org-babel-load-languages
- '((emacs-lisp . t)
-   (java . t)))
+ '(
+   (emacs-lisp . t)
+   (java . t)
+   (haskell . t)
+   (latex . t)
+   ))
 
+;; (setq max-lisp-eval-depth 10000)
+;; (setq max-specpdl-size 10000)
 
 (provide 'init-org-literate)
